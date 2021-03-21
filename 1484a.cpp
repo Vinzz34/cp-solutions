0287c0b845b10c513609161601599a80330214d4
Vinzz34 <vinaygn29@gmail.com>
2021-03-21 22:19:03 +0530

    first codeforces sol

---
+++ 1484a.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ar array
+#define ld long double
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define PB push_back
+#define MP make_pair
+#define fi first
+#define se second
+#define sz(a) int((a).size())
+#define FOR(a,b,c) for(int a=b;a<(int)(c);a++)
+#define MOD 1000000007
+#define EACH(v) for(auto x : v)
+#define VINZZ
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b;
+		cin>>a>>b;
+		cout<<a*b<<endl;
+	}
+	#ifdef VINZZ
+	    cerr<<"Time: "<<1.0*clock()/CLOCKS_PER_SEC<<"s.\n";
+	#endif
+	return 0;
+}
+
