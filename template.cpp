a0e0e1dcc721d56b986c69d2beb6f1c6c4a44911
Vinzz <vinaygn29@gmail.com>
2022-07-28 22:28:40 +0530

    template

---
+++ template.cpp
@@ -0,0 +1,24 @@
+/**
+ *   author: Vinzz34
+**/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ar array
+#define ld long double
+#define vi vector<int>
+#define vpii vector<pair<int,int>>
+#define PB push_back
+#define MP make_pair
+#define fi first
+#define se second
+#define sz(a) int((a).size())
+#define FOR(a,b,c) for(int a=b;a<(int)(c);a++)
+#define MOD 1e9+7
+#define EACH(x,a) for(auto& x : a)
+#define VINZZ
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	return 0;
+}
---
+++ template1.cpp
@@ -0,0 +1,59 @@
+/**
+ *   author: Vinzz34
+**/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ar array
+#define vt vector
+#define vpii vector<pair<int,int>>
+#define PB push_back
+#define MP make_pair
+#define fi first
+#define se second
+#define sz(a) int((a).size())
+#define MOD 1000000007
+#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define EACH(x, a) for (auto& x: a)
+#define VINZZ
+
+template<class A> void read(vt<A>& v);
+template<class A, size_t S> void read(ar<A, S>& a);
+template<class T> void read(T& x) {
+	cin >> x;
+}
+void read(double& d) {
+	string t;
+	read(t);
+	d=stod(t);
+}
+void read(long double& d) {
+	string t;
+	read(t);
+	d=stold(t);
+}
+template<class H, class... T> void read(H& h, T&... t) {
+	read(h);
+	read(t...);
+}
+template<class A> void read(vt<A>& x) {
+	EACH(a, x)
+		read(a);
+}
+template<class A, size_t S> void read(array<A, S>& x) {
+	EACH(a, x)
+		read(a);
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	return 0;
+}
+
